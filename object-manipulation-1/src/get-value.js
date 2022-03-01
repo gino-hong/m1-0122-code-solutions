69515b8371ec4ca7d71df86855a2d44121cafc08
Gino Hong <ginohong7@gmail.com>
2022-02-28 22:40:21 -0800

    Completed object-manipulation-1.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,8 @@
+/* exported getValue */
+
+// define a function named getValue with two parameters 'object' and 'key'
+// return the value of the object at key
+
+function getValue(object, key) {
+  return object[key];
+}
---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,8 @@
+/* exported setValue */
+
+// define a function named setValue with three parameters 'object', 'key', and 'value'
+// assign value to the object at key
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
