67547bee460300b53017b57a85a711f532126d23
Gino Hong <ginohong7@gmail.com>
2022-03-15 19:46:42 -0700

    Defined object locally.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,10 +1,10 @@
 var $contactForm = document.querySelector('#contact-form');
-var messageData = {};
 
 $contactForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+  var messageData = {};
   messageData.name = $contactForm.elements.name.value;
   messageData.email = $contactForm.elements.email.value;
   messageData.message = $contactForm.elements.message.value;
